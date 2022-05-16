abf231ebad15cac3d3b1e792846c5e228c6a14e7
Helder Davidson Rodrigues Alvarenga <70406114+Uken49@users.noreply.github.com>
2022-05-16 13:03:33 -0300

    Adicionando favicon e narração
    Criando a narração usando js, falta colocar o texto final para finalizar.

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,10 @@
 document.getElementsByTagName('body')[0].addEventListener('click', toMenu)
+var content = document.getElementById('content_1')
 
-function toMenu(){
+// INICIO - Primeiro conteúdo (Imagem e narração)
+
+// Desaparencendo a imagem
+function toMenu() {
     let logo = document.getElementById('fig_inicio')
 
     logo.style.opacity = "0"
@@ -13,14 +17,35 @@ function toMenu(){
     }, 2000);
 }
 
+// Aparecendo a tag que vai estar a narração
 function content1() {
-    let content = document.getElementById('content_1')
-
     content.style.display = "flex"
-
-    setTimeout(() => {
-        if (content.parentNode) {
-            content.style.opacity = "1"
-        }
-    }, 2000);
+    narrative()
 }
+
+// Criando delay pra narração
+const timer = (seconds) => {
+    let time = seconds * 1000
+    return new Promise(res => setTimeout(res, time))
+}
+
+// Exibindo a narração
+async function narrative() {
+    let storyteller = document.getElementById('span_storyteller')
+    let phrase = [`Testando`, `Testando de novo`, `Demora né?`, `Legal essa fonte, né?`, `Estamos aqui à tanto tempo`
+                    ,`Testando`, `Me prepare um café`, `Em breve...os Yonkous irão surgir`]
+
+    for (let i = 0; i < phrase.length; i++) {
+        let seconds = i == 0 ? 0.3 : 2
+
+        await timer(seconds);
+        content.style.opacity = "1"
+        storyteller.innerHTML = phrase[i]
+        await timer(4);
+        content.style.opacity = "0"
+    }
+
+    //Próximo passo
+}
+
+// FIM - Primeiro conteúdo (Imagem e narração)
