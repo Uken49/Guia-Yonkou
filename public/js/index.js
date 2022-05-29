065dfdbef5227944c288dab35b796490fa144c4f
uken49 <heldergavioes@hotmail.com>
2022-05-28 21:32:47 -0300

    Inicio - Narração: Colocando sessão Modificando a estrutura do código e botando sessão

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,32 @@
-var body = document.getElementsByTagName('body')[0]
-body.addEventListener('click', toMenu)
-var content_1 = document.getElementById('content_1')
+// Criando sessão para sumir com narrativa após primeira visualização
+var body = document.getElementsByTagName("body")[0]
+
+// Parte de narração
+var cookie = localStorage.getItem("storyteller")
+var inicio = document.getElementById('content_1')
+var logoStory = document.getElementById('fig_logo')
+
+// Parte "normal" do site
+var header = document.getElementById("header")
+var content2 = document.getElementById("content_2")
+
+body.addEventListener('load', checkCookie)
+inicio.addEventListener('click', toMenu)
+var content1start = document.getElementById('art_storyteller')
+
+
+function checkCookie() {
+    // Verificando cookie
+    if (cookie) {
+        inicio.parentNode.removeChild(inicio)
+        content1start.parentNode.removeChild(content1start)
+        header.style.visibility = "visible"
+        content2.style.visibility = "visible"
+
+        header.style.opacity = "1"
+        content2.style.opacity = "1"
+    }
+}
 
 // INICIO - Primeiro conteúdo (Imagem e narração)
 
@@ -9,13 +35,12 @@ particlesJS.load('particles-container', 'particlesjs-config.json');
 
 // Desaparencendo a imagem
 function toMenu() {
-    let logo = document.getElementById('fig_inicio')
 
-    logo.style.opacity = "0"
+    logoStory.style.opacity = "0"
 
     setTimeout(() => {
-        if (logo.parentNode) {
-            logo.parentNode.removeChild(logo);
+        if (logoStory.parentNode) {
+            logoStory.parentNode.removeChild(logoStory);
             content1()
         }
     }, 2000);
@@ -23,7 +48,7 @@ function toMenu() {
 
 // Aparecendo a tag que vai estar a narração
 function content1() {
-    content_1.style.display = "flex"
+    content1start.style.display = "flex"
     narrative()
 }
 
@@ -37,40 +62,40 @@ const timer = (seconds) => {
 async function narrative() {
     let storyteller = document.getElementById('span_storyteller')
     let phrase = [
-        'Lendas, lendas', 'Vagam pelo mar', 'Irão se assustar', 'Frente a um Yonkou',
-        'Lendas, lendas', 'Sempre a navegar', 'Caso os encontrar', 'Saiba sua vida acabou',
-        'Boatos sobre esses seres', 'Saiba se espalhou', 'Governando suas ilhas', 'São os...',
+        // 'Lendas, lendas', 'Vagam pelo mar', 'Irão se assustar', 'Frente a um Yonkou',
+        // 'Lendas, lendas', 'Sempre a navegar', 'Caso os encontrar', 'Saiba sua vida acabou',
+        // 'Boatos sobre esses seres', 'Saiba se espalhou', 'Governando suas ilhas', 'São os...',
         'Yonkous']
 
     for (let i = 0; i < phrase.length; i++) {
         let seconds = i == 0 ? 0.3 : 2
 
         await timer(seconds);
-        content_1.style.opacity = "1"
+        content1start.style.opacity = "1"
         storyteller.innerHTML = phrase[i]
         await timer(3);
-        content_1.style.opacity = "0"
+        content1start.style.opacity = "0"
     }
 
     // Tirando conteúdo 1 e surgindo conteúdo 2
     setTimeout(() => {
-        let header = document.getElementById("header")
-        let content2 = document.getElementById("content_2")
+        var header = document.getElementById("header")
+        var content2 = document.getElementById("content_2")
 
         // Sumindo com a narração 
-        setTimeout(() => {
-            content_1.style.opacity = "0"
-        }, 500);
-        content_1.style.visibility = "hidden"
+        inicio.style.opacity = "0"
+        setTimeout(() => { inicio.parentNode.removeChild(inicio); }, 2000);
 
         // Aparecendo o site
-        body.style.backgroundColor = "#fff"
-
         header.style.visibility = "visible"
         content2.style.visibility = "visible"
 
         header.style.opacity = "1"
         content2.style.opacity = "1"
+
+        // Cria o cookie
+        localStorage.setItem("storyteller", "1")
+
     }, 2000);
 
 }
